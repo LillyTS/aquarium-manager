95cbb2402f2d5a69a7d88b031e0a8f769f185ef5
Liliana Santos <liliana.t.santos@hotmail.com>
2022-09-27 04:02:01 +0100

    Getting info from db and displaying it
    - Getting info from only one static document on firebase
    - Changed styles

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, child, get } from 'firebase/database';
+// import { getAuth } from "firebase/auth";
+
+const myUser: string = 'heQyO6VnDeazONcYLgBQVa6GfEJ2';
 
 @Component({
   selector: 'app-card',
@@ -11,22 +14,65 @@ export class CardComponent implements OnInit {
   title: string;
   dimensions: Dimensions;
   liters: number | null;
+  fish: string | null;
+  snails: string | null;
+  shrimps: string | null;
+  plants: string | null;
 
   constructor() {
     this.title = 'Aquarium';
     this.dimensions = {
-      height: 10,
-      width: 10,
-      length: 10,
+      height: null,
+      width: null,
+      length: null,
     };
     this.liters = null;
+    this.fish = '';
+    this.snails = '';
+    this.shrimps = '';
+    this.plants = '';
   }
 
   ngOnInit(): void {
+    //const auth = getAuth();
+
     const dbRef = ref(getDatabase());
-    get(child(dbRef, `/users/heQyO6VnDeazONcYLgBQVa6GfEJ2`))
+    get(child(dbRef, `/users/${myUser}/aquariums`))
       .then((snapshot) => {
         if (snapshot.exists()) {
+          const aquarium = snapshot.val().aquarium1;
+          this.title = aquarium.name;
+          Object.keys(aquarium.fish).forEach(
+            (name: string) =>
+              (this.fish += ` ${name} (${aquarium.fish[name].quantity})\n`)
+          );
+
+          Object.keys(aquarium.snails).forEach(
+            (name: string) =>
+              (this.snails += ` ${name} (${aquarium.snails[name].quantity})\n`)
+          );
+          Object.keys(aquarium.shrimps).forEach(
+            (name: string) =>
+              (this.shrimps += ` ${name} (${aquarium.shrimps[name].quantity})\n`)
+          );
+          Object.keys(aquarium.plants).forEach(
+            (name: string) => (this.plants += ` ${aquarium.plants[name]}\n`)
+          );
+
+          this.dimensions.height = aquarium.dimensions.height;
+          this.dimensions.width = aquarium.dimensions.width;
+          this.dimensions.length = aquarium.dimensions.length;
+          if (
+            this.dimensions.height &&
+            this.dimensions.width &&
+            this.dimensions.length
+          ) {
+            this.liters =
+              (this.dimensions.height *
+                this.dimensions.width *
+                this.dimensions.length) /
+              1000;
+          }
           console.log(snapshot.val());
         } else {
           console.log('No data available');
