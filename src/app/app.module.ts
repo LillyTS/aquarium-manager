0bb572b35b6e53e89921cfa10cea18f948656adf
Liliana Santos <liliana.t.santos@hotmail.com>
2022-09-29 17:13:05 +0100

    Created a card add component
    This component will open a pop up to add a new aquarium (not yet implemented)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,9 +15,10 @@ import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { CardAddComponent } from './card-add/card-add.component';
 
 @NgModule({
-  declarations: [AppComponent, CardComponent],
+  declarations: [AppComponent, CardComponent, CardAddComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ src/app/card-add/card-add.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-add',
+  templateUrl: './card-add.component.html',
+  styleUrls: ['./card-add.component.scss'],
+})
+export class CardAddComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
